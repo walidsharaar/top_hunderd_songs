e2a779693f6a7aef70e9bc163a1c2ce7e18bbb6d
Sharaar <walidsharar@gmail.com>
2021-07-22 21:56:50 +0200

    fetched all data from the website

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://www.billboard.com/charts/hot-100"
+#ask user input
+user_date = input("Which year's song do you want to listen? Please enter the date in this format YYYY-MM-DD\n")
+
+print(user_date)
+
+# Scrape top 100 song from https://www.billboard.com/charts/hot-100
+
+reponse = requests.get(URL)
+print(reponse.text)
