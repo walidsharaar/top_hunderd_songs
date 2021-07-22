4e19cbc39888040fa6b0c745e0d93e4be2468503
Sharaar <walidsharar@gmail.com>
2021-07-22 22:19:10 +0200

    get 100 songs from the website and stores in a text file.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-URL = "https://www.billboard.com/charts/hot-100"
-#ask user input
-user_date = input("Which year's song do you want to listen? Please enter the date in this format YYYY-MM-DD\n")
+URL = "https://www.billboard.com/charts/hot-100/"
+user_date = input("What day in the past do you want to travel back? Type YYYY-MM-DD ")
+response = requests.get(f"{URL}{user_date}")
+billboard = response.text
+soup = BeautifulSoup(billboard, "html.parser")
+playlist_song = soup.find_all(name='span', class_='chart-element__information__song text--truncate color--primary')
+playlist_song = [(item.getText()) for item in playlist_song]
+playlist_author = soup.find_all(name='span', class_='chart-element__information__artist text--truncate color--secondary')
+playlist_author = [(item.getText()) for item in playlist_author]
 
-print(user_date)
+complete_list = [(item1+" - "+item2) for (item1, item2) in zip(playlist_author, playlist_song)]
 
-# Scrape top 100 song from https://www.billboard.com/charts/hot-100
-
-reponse = requests.get(URL)
-print(reponse.text)
+with open(f"{user_date}_playlist.txt", mode="w") as file:
+    for song in complete_list:
+        file.write(f"{song}\n")
